0a0cc4c7d6617242439d764b9b2774ca1ddd503b
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-02-23 21:33:48 +0000

    Update exceRpt.Slurm.Array.sh

--- exceRpt/exceRpt.Slurm.Array.sh
+++ exceRpt/exceRpt.Slurm.Array.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -A Research_Project-MRC164847 # research project to submit under.
-#SBATCH --job-name=LDpred2  # job name
+#SBATCH --job-name=exceRpt  # job name
 #SBATCH --output=exceRpt.%j.log  # output
 #SBATCH --export=ALL # export all environment variables to the batch job.
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq # submit to the parallel test queue
-#SBATCH --time=5:00:00 # Maximum wall time for the job
+#SBATCH --time=25:00:00 # Maximum wall time for the job
 #SBATCH --nodes=1 # specify number of nodes.
 #SBATCH --ntasks-per-node=16 # specify number of processors.
 #SBATCH --mail-type=END # send email at job completion
