4f714b60493a93937d55ee5810234c328b96f39a
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2025-01-22 14:59:29 +0000

    Update preparing_miRNA_count.R

--- mirdeep2/preparing_miRNA_count.R
+++ mirdeep2/preparing_miRNA_count.R
@@ -1,38 +1,82 @@
 library(stringr)
-library(vroom)
+
 args <- commandArgs(T)
 
-#input_dir <- args[1]
-expr_file <- args[1]
-config_file <- args[2]
-out_prefix <- args[3]
+mirdeep.results.dir <- args[1] # The directory contains mirdeep2 results
+OutPrefix <- args[2]           # This script Output files prefix
+SampleID.trim <- args[3]       # The substring that you want to remove from the samples IDs in the count matrix (e.g. ".fastq")
+
+if(is.na(OutPrefix)){
+  OutPrefix = ""
+}else{
+  OutPrefix = paste0(OutPrefix , ".")
+}
+
+if(is.na(mirdeep.results.dir)){
+  mirdeep.results.dir = "."
+}
+
+message("mirdeep2 results directory: ", mirdeep.results.dir)
+message("Count matrix files prefix: ", OutPrefix)
+message("The following substring will be removed from the sample IDs: ", SampleID.trim)
+cat("\n")
+
+message("Detecting expression files...")
+
+exprs.files = data.frame(ExprFile = list.files(path = mirdeep.results.dir , pattern = "^miRNAs_expressed_all_samples_.*[.]csv",
+                                               all.files = F,full.names = F, recursive = F,ignore.case = F))
+if(nrow(exprs.files) == 0){
+  stop("No expression file were detected. Check the results directory: ",mirdeep.results.dir)
+}else{
+  message(nrow(exprs.files), " expression files were detected:")
+  cat("\n")
+}
+exprs.files$ConfigFile = NA
+counts = vector(mode = "list" , length = nrow(exprs.files))
+
+for (i in 1:nrow(exprs.files)) {
+  
+  message("Working on expression file ",i,"...")
+
+  mirdeep.name = str_remove(exprs.files$ExprFile[i] , pattern = ".csv")
+  mirdeep.name = str_remove(mirdeep.name , pattern = "miRNAs_expressed_all_samples_")
+  config.path = paste0(mirdeep.results.dir , "/expression_analyses/expression_analyses_",mirdeep.name)
+  f = list.files(path = config.path , pattern = ".*fa_mapped.arf",
+                all.files = F,full.names = F, recursive = F,ignore.case = F)
+  exprs.files$ConfigFile[i] = paste0(str_remove(f , pattern = "fa_mapped.arf"),"config")
 
 
-config <- read.delim(file = config_file,header = F,stringsAsFactors = F,colClasses = c("character", "character"))
 
-#files <- list.files(path=input_dir,pattern="miRNAs_expressed_all_samples_(.)*.csv", full.names = T)
+  config_file = paste0(mirdeep.results.dir , "/",exprs.files$ConfigFile[i])
+  expr_file = paste0(mirdeep.results.dir , "/",exprs.files$ExprFile[i])
 
-#f <- read.delim(files[1])[,c(-2,-4)]
-f <- read.delim(expr_file)
-names(f) <- str_remove(names(f) , pattern = "X")
-index <- str_detect(names(f) , pattern = "norm")
-f <- f[,!index]
+  config <- read.delim(file = config_file,header = F,stringsAsFactors = F,colClasses = c("character", "character"))
+  expr <- read.delim(expr_file)
 
-#expr <- list()
-#expr[[1]] <- f
+  names(expr) <- str_remove(names(expr) , pattern = "X")
+  index <- str_detect(names(expr) , pattern = "norm")
+  expr <- expr[,!index]
 
-#for (i in 2:length(files)) {
- # f <- read.delim(files[i])[,c(-1:-4)]
-  #names(f) <- str_remove(names(f) , pattern = "X")
-  #index <- str_detect(names(f) , pattern = "norm")
-  #f <- f[,!index]
-  #expr[[i]] <- f
-#}
-#expr1 <- do.call(cbind.data.frame,expr)
-expr1 <- f
+  config[,1] <- basename(config[,1])
+  index <- match(names(expr)[c(-1:-4)],config[,2])
+  names(expr)[c(-1:-4)] <- config[index , 1]
+  
+  if(!is.na(SampleID.trim)){
+    names(expr) = str_remove(names(expr) , pattern = SampleID.trim)
+  }
+  
+  write.csv(expr , file = paste0(dirname(OutPrefix),"/",exprs.files$ConfigFile[i],".count.csv"),row.names = F)
+  mirna.ids = expr[,c(1,3)]
+  names(mirna.ids) = c("miRNA" , "Precursor")
+  expr = expr[,c(-1:-4)]
+  counts[[i]] = expr
+}
 
-config[,1] <- basename(config[,1])
-index <- match(names(expr1)[c(-1:-4)],config[,2])
-names(expr1)[c(-1:-4)] <- config[index , 1]
 
-write.csv(expr1 , file = paste0(out_prefix,".csv"),row.names = F)
+write.csv(exprs.files , file = paste0(OutPrefix , "mirdeep2.Config.Matched.csv"), row.names = F)
+
+counts.merged = do.call(cbind.data.frame , c(mirna.ids , counts))
+
+message("Writing merged count matrix...")
+write.csv(counts.merged , file = paste0(OutPrefix,"mirdeep2.count.csv"),row.names = F)
+
