7de7a46fd6f25b09a016111ecd7fe4219bb67511
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2025-02-07 10:09:17 +0000

    Update Combined.PairedEnd.sh

--- mirdeep2/Combined.PairedEnd.sh
+++ mirdeep2/Combined.PairedEnd.sh
@@ -1,11 +1,1 @@
-InputDir=./
-OutDir=./
 
-for i1 in ${InputDir}/*R1*.fastq.gz
-do
-	i2=${i1/R1/R2}
-	name=${i1/R1/R12}
-  name=$(basename -- "$name")
-	echo $name
-	cat $i1 $i2  > ${OutDir}/$name
-done
