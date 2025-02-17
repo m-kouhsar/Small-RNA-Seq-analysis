22cc628451c886e79f7f867342aca153f7eb74fc
Kouhsar <mk693@login01.cluster.local>
2025-02-17 11:02:51 +0000

    Using Pear tool to merge paired end data

--- mirdeep2/Combined.PairedEnd.sh
+++ mirdeep2/Combined.PairedEnd.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#SBATCH -A Research_Project1 # research project to submit under.
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -D . # set working directory to .
+#SBATCH -p mrcq
+#SBATCH --time=5:00:00 # Maximum wall time for the job
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --ntasks-per-node=16 # specify number of processors.
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
+
+R1=$1
+R2=$2
+Out=$3
+
+pear -f $R1 -r $R2 -o $Out.Pear.Merged
+
 
