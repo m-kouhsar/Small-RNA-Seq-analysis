da603d3e28844768e43bb904edd37c281e2ae881
Kouhsar <mk693@login01.cluster.local>
2025-03-14 11:36:03 +0000

    Updated

--- mirdeep2/mirdeep2.report.R
+++ mirdeep2/mirdeep2.report.R
@@ -21,35 +21,27 @@ cat("\n")
 
 message("Detecting expression files...")
 
-exprs.files = data.frame(ExprFile = list.files(path = mirdeep.results.dir , pattern = "^miRNAs_expressed_all_samples_.*[.]csv",
-                                               all.files = F,full.names = F, recursive = T,ignore.case = F))
+files_ <- list.files(path = mirdeep.results.dir , pattern = "^miRNAs_expressed_all_samples_.*[.]csv",
+                     all.files = F,full.names = T, recursive = T,ignore.case = F)
+exprs.files = data.frame(ExprFile = basename(files_),Path =dirname( files_ ))
 if(nrow(exprs.files) == 0){
   stop("No expression file were detected. Check the results directory: ",mirdeep.results.dir)
 }else{
   message(nrow(exprs.files), " expression files were detected:")
   cat("\n")
 }
-exprs.files$ConfigFile = NA
+
+exprs.files$ConfigFile <- NA
 counts = vector(mode = "list" , length = nrow(exprs.files))
 
 for (i in 1:nrow(exprs.files)) {
   
   message("Working on expression file ",i,"...")
-
-  mirdeep.name = str_remove(exprs.files$ExprFile[i] , pattern = ".csv")
-  mirdeep.name = str_remove(mirdeep.name , pattern = "miRNAs_expressed_all_samples_")
-  config.path = paste0(mirdeep.results.dir , "/expression_analyses/expression_analyses_",mirdeep.name)
-  f = list.files(path = config.path , pattern = ".*fa_mapped.arf",
-                all.files = F,full.names = F, recursive = F,ignore.case = F)
-  exprs.files$ConfigFile[i] = paste0(str_remove(f , pattern = "fa_mapped.arf"),"config")
-
-
-
-  config_file = paste0(mirdeep.results.dir , "/",exprs.files$ConfigFile[i])
-  expr_file = paste0(mirdeep.results.dir , "/",exprs.files$ExprFile[i])
-
-  config <- read.delim(file = config_file,header = F,stringsAsFactors = F,colClasses = c("character", "character"))
-  expr <- read.delim(expr_file)
+  
+  exprs.files$ConfigFile[i]= basename(list.files(path =exprs.files$Path[i],pattern = "config.*.txt",all.files = F,full.names = F, recursive = T,ignore.case = F))
+  
+  config <- read.delim(file = paste0(exprs.files$Path[i] , "/",exprs.files$ConfigFile[i]),header = F,stringsAsFactors = F,colClasses = c("character", "character"))
+  expr <- read.delim(paste0(exprs.files$Path[i] , "/",exprs.files$ExprFile[i]))
 
   names(expr) <- str_remove(names(expr) , pattern = "X")
   index <- str_detect(names(expr) , pattern = "norm")
@@ -59,7 +51,7 @@ for (i in 1:nrow(exprs.files)) {
   index <- match(names(expr)[c(-1:-4)],config[,2])
   names(expr)[c(-1:-4)] <- config[index , 1]
   
-  write.csv(expr , file = paste0(dirname(OutPrefix),"/",exprs.files$ConfigFile[i],".count.csv"),row.names = F)
+  # write.csv(expr , file = paste0(dirname(OutPrefix),"/",exprs.files$ConfigFile[i],".count.csv"),row.names = F)
   mirna.ids = expr[,c(1,3)]
   names(mirna.ids) = c("miRNA" , "Precursor")
   expr = expr[,c(-1:-4)]
