49110c43fd04a32a3ac2e31e7a668f7f77566d99
Kouhsar <mk693@login02.cluster.local>
2025-07-25 00:14:38 +0100

    Updated

--- ShortStack/shortstack.sh
+++ ShortStack/shortstack.sh
@@ -3,7 +3,7 @@
 #SBATCH --export=ALL # export all environment variables to the batch job.
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq
-#SBATCH --time=30:00:00 # Maximum wall time for the job
+#SBATCH --time=10:00:00 # Maximum wall time for the job
 #SBATCH --nodes=1 # specify number of nodes.
 #SBATCH --ntasks-per-node=16 # specify number of processors.
 #SBATCH --mail-type=END # send email at job completion
@@ -11,13 +11,10 @@
 
 #########################################################################################
 #########################################################################################
-# Installing MINTmap: 'conda install bioconda::mintmap'
-# To see the parameters description run 'MINTmap.pl -h'
 
 result_dir=./ShortStack.Results
 fastq_dir=./sRNA_Seq_trimmed
 genome_fasta="/lustre/projects/Research_Project-191391/Morteza/Small_nonCoding_RNAs/miRNA/mirdeep2/Ref/hg38.fa"
-threads=16
 
 #######################################################################################
 #######################################################################################
@@ -32,7 +29,7 @@ do
     sample_name=$(basename $i)
     echo "working on sample $((j+1)): $sample_name"
     
-    ShortStack --genomefile $genome_fasta --readfile $i --outdir ${result_dir}/${sample_name%.fastq.gz} --threads $threads
+    ShortStack --genomefile $genome_fasta --readfile $i --outdir ${result_dir}/${sample_name%.fastq.gz}
     
     j=$(( j + 1 ))
 done
