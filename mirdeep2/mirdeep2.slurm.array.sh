d12577b1e23441f460e560eb8cd73c25e2b32893
Kouhsar <mk693@login02.cluster.local>
2025-10-15 16:08:28 +0100

    Updated

--- mirdeep2/mirdeep2.slurm.array.sh
+++ mirdeep2/mirdeep2.slurm.array.sh
@@ -3,26 +3,28 @@
 #SBATCH --export=ALL # export all environment variables to the batch job.
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq
-#SBATCH --time=50:00:00 # Maximum wall time for the job
+#SBATCH --time=10:00:00 # Maximum wall time for the job
 #SBATCH --nodes=1 # specify number of nodes.
 #SBATCH --ntasks-per-node=16 # specify number of processors.
 #SBATCH --mail-type=END # send email at job completion
 #SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
-#SBATCH --array=0-19
+#SBATCH --job-name=mirdeep2
+#SBATCH --output=mirdeep2.%j.out
+#SBATCH --array=0-15
 #########################################################################################
 #########################################################################################
 
-result_dir=./mirdeep2.results
-fastq_dir=./trimmed_fastq_files
-genome_file=./hg38.fa
-bowtie_index_pref=./bowtie-index/hg38.fa
-hairpin_file=./hairpin1.fa.fix
-mature_file=./mature1.fa.fix
+result_dir="./mirdeep2/BDR1"
+fastq_dir="./Project.10230.BDR1"
+genome_file="./Ref/hg38.fa"
+bowtie_index_pref="./Ref/hg38"
+hairpin_file="./Ref/hairpin1.fa.fix"
+mature_file="./Ref/mature1.fa.fix"
 
 #######################################################################################
 #######################################################################################
 
-fastq_files=(${fastq_dir}/*R1*.fastq)
+fastq_files=(${fastq_dir}/*R1*.fastq*)
 
 Num_samp=${#fastq_files[@]}
 
@@ -38,6 +40,10 @@ then
     fastq_files1=(${fastq_files[@]:$lower})
 fi
 
+echo "##########################################################################"
+echo "                    Running job array $SLURM_ARRAY_TASK_ID                "
+echo "##########################################################################"
+
 echo Output directory: $result_dir
 echo Fastq files directory: $fastq_dir
 echo Genome file for mirdeep2: $genome_file
@@ -49,22 +55,27 @@ echo current array index: $SLURM_ARRAY_TASK_ID
 echo Number of samples in current array: ${#fastq_files1[@]}
 
 echo "##########################################################################"
-echo -e '\n'
+echo ""
 result_dir=${result_dir}/mirdeep2.results.${SLURM_ARRAY_TASK_ID}
 if [ ${#fastq_files1[@]} != 0 ]
 then
     mkdir -p $result_dir
     cd $result_dir
-    #step 1: generating config file
-    echo Saving config file to ${result_dir}/config.${SLURM_ARRAY_TASK_ID}.txt
+    #step 1: creating config file
+    echo ""
+    echo "[STEP1] Creating config file: ${result_dir}/config.${SLURM_ARRAY_TASK_ID}.txt"
+    echo ""
     j=0
     for i in ${fastq_files1[@]}
     do
         if [[ $i == *.gz ]]
         then
-            echo "unzipping $i ..."
-            gunzip $i
-            i="${i%.gz}"
+            echo "unzipping $(basename $i) ..."
+            i1=${result_dir}/$(basename "$i")
+            i1="${i1%.gz}"
+            echo "Writing unzipped file to $(dirname $i1) ..."
+            gunzip -c $i > $i1
+            i=$i1
         fi
         j_code=$(printf "%03d\n" "$((j+1))")
         echo -e "${i}\t${j_code}" >> config.${SLURM_ARRAY_TASK_ID}.txt
@@ -74,25 +85,29 @@ then
     config_file=config.${SLURM_ARRAY_TASK_ID}.txt
 
     #step 2: mapping on the ref genome using mapper module
-    echo -e '\n'
-    echo "Running mapper.pl..."
-
+    echo ""
+    echo "[STEP2] Running mapper.pl..."
+    echo ""
     mapper.pl ${config_file} \
         -d -e -h -i -j  -l 18 -m -p $bowtie_index_pref \
         -s mapper.${SLURM_ARRAY_TASK_ID}.fa \
         -t mapper.${SLURM_ARRAY_TASK_ID}.arf -v -o 16\
 
     #step 3: extracting miRNA count with miRdeep2 module
-    echo -e '\n'
-    echo "Running miRDeep2.pl..."
-
+    echo ""
+    echo "[STEP3] Running miRDeep2.pl..."
+    echo ""
     miRDeep2.pl mapper.${SLURM_ARRAY_TASK_ID}.fa \
         ${genome_file} \
         mapper.${SLURM_ARRAY_TASK_ID}.arf \
         ${mature_file} none ${hairpin_file} \
         -t hsa 2>mirdeep2.${SLURM_ARRAY_TASK_ID}.log \
 
-    echo "all processes have been done!"
+    echo ""
+    echo "All done!"
+    echo ""
 else
+    echo ""
     echo "There is no sample in this array!"
+    echo ""
 fi
